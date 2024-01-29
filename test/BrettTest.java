4b95d339cced335a65248ec2f43d0d8d28a63ee1
h671440 <671440@stud.hvl.no>
2024-01-29 12:56:56 +0100

    bretttest

---
+++ test/BrettTest.java
@@ -0,0 +1,50 @@
+import javax.annotation.processing.SupportedAnnotationTypes;
+import static org.junit.Assert.assertEquals;
+
+public class BrettTest {
+
+    private Brett brett;
+
+    @Before
+    public void setUp(){
+        brett = new Brett();
+    }
+
+    @Test
+
+    public void testGetStartRute(){
+        Rute startRute = brett.getStartRute();
+        assertEquals(0, startRute.getIndex());
+    }
+
+    @Test
+    public void TestRuteUtenSlangeEllerStige(){
+        int squarenumber = 5;
+        int result = brett.checkSquare(squarenumber);
+        assertEquals(squarenumber, result);
+    }
+
+    @Test
+    public void testRuteMedStige(){
+        int rutenr = 3;
+        int resultat = brett.checkSquare(rutenr);
+        assertEquals(rutenr, resultat;
+    }
+
+    @Test
+    public void TestRutemedSLange(){
+        int squarenr = 90;
+        int ankommetrute = 76;
+        int resultat = brett.checkSquare(squarenr);
+        assertEquals(ankommetrute, resultat);
+    }
+
+
+
+
+
+
+
+
+
+}
