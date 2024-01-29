7087dbaff6b225fa6e3cf5ffa41c5adbc97375e8
borz <adamyasaev7@gmail.com>
2024-01-29 12:39:20 +0100

    lager test

---
+++ test/TerningTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class TerningTest {
+    private Terning terning;
+
+    @BeforeEach
+    void setUp() {
+        terning = new Terning();
+    }
+
+}
